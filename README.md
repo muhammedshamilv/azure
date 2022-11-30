1800651092a4928bb31b92b47dafd30b215f210b
muhammedshamilv <shamilrjv@gmail.com>
2022-11-30 16:25:26 +0530

    crud operation for resource groups in azure and created virtual machine

---
+++ README.md
@@ -0,0 +1,8 @@
+Run
+```
+pip install -r requirements.txt
+```
+1.Login via terminal using az login
+2.After try az account show
+3.Copy id and set it as subscription_id
+4.run the file
---
+++ virtualmachine.py
@@ -0,0 +1,132 @@
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+
+credential = AzureCliCredential()
+
+subscription_id = "183f18e4-754f-4be4-82d6-94ae8dd571aa"
+
+resource_client = ResourceManagementClient(credential, subscription_id)
+
+RESOURCE_GROUP_NAME = "test"
+LOCATION = "eastus"
+
+#create resourse group
+
+rg_result = resource_client.resource_groups.create_or_update(RESOURCE_GROUP_NAME,
+    {
+        "location": LOCATION
+    }
+)
+
+print(f"Resource group {rg_result} ")
+
+#create a virtual network
+
+VNET_NAME = "vnet"
+SUBNET_NAME = "subnet"
+IP_NAME = "ip"
+IP_CONFIG_NAME = "ip-config"
+NIC_NAME = "nic"
+
+network_client = NetworkManagementClient(credential, subscription_id)
+
+poller = network_client.virtual_networks.begin_create_or_update(RESOURCE_GROUP_NAME,
+    VNET_NAME,
+    {
+        "location": LOCATION,
+        "address_space": {
+            "address_prefixes": ["10.0.0.0/16"]
+        }
+    }
+)
+
+vnet_result = poller.result()
+
+print(f"virtual network {vnet_result}")
+
+# create subnet from the virtual network
+
+poller = network_client.subnets.begin_create_or_update(RESOURCE_GROUP_NAME, 
+    VNET_NAME, SUBNET_NAME,
+    { "address_prefix": "10.0.0.0/24" }
+)
+subnet_result = poller.result()
+
+print(f"subnet {subnet_result}")
+
+# create IP address 
+
+poller = network_client.public_ip_addresses.begin_create_or_update(RESOURCE_GROUP_NAME,
+    IP_NAME,
+    {
+        "location": LOCATION,
+        "sku": { "name": "Standard" },
+        "public_ip_allocation_method": "Static",
+        "public_ip_address_version" : "IPV4"
+    }
+)
+
+ip_address_result = poller.result()
+
+print(f"IP address {ip_address_result}")
+
+# create network interface
+
+poller = network_client.network_interfaces.begin_create_or_update(RESOURCE_GROUP_NAME,
+    NIC_NAME, 
+    {
+        "location": LOCATION,
+        "ip_configurations": [ {
+            "name": IP_CONFIG_NAME,
+            "subnet": { "id": subnet_result.id },
+            "public_ip_address": {"id": ip_address_result.id }
+        }]
+    }
+)
+
+nic_result = poller.result()
+
+print(f"network interface client {nic_result}")
+
+# create virtual machine
+
+compute_client = ComputeManagementClient(credential, subscription_id)
+
+VM_NAME = "TestVM"
+USERNAME = "azureuser"
+PASSWORD = "user@123"
+
+print(f"creating virtual machine will take some time")
+
+poller = compute_client.virtual_machines.begin_create_or_update(RESOURCE_GROUP_NAME, VM_NAME,
+    {
+        "location": LOCATION,
+        "storage_profile": {
+            "image_reference": {
+                "publisher": 'hamon',
+                "offer": "UbuntuServer",
+                "sku": "16.04.0-LTS",
+                "version": "latest"
+            }
+        },
+        "hardware_profile": {
+            "vm_size": "Standard_DS1_v2"
+        },
+        "os_profile": {
+            "computer_name": VM_NAME,
+            "admin_username": USERNAME,
+            "admin_password": PASSWORD
+        },
+        "network_profile": {
+            "network_interfaces": [{
+                "id": nic_result.id,
+            }]
+        }        
+    }
+)
+
+vm_result = poller.result()
+
+print(f"virtual machine {vm_result.name}")
